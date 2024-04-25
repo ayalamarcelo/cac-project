8586b6335374ae9e4fedbcf0b5bcc8b4765ae62f
Marcelo <marcelorubenayala@outlook.com>
2024-04-25 12:57:00 -0300

    Refactor code :recycle:

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-let img1 = "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/30b04167574805.5b3e42ef3d364.jpg";
-let img2 = "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/e601ef67574805.5b3e42ef3e2cb.jpg";
-let img3 = "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/e8cd9b67574805.5b3e42ef3de3b.jpg";
+let imgUrls = [
+    "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/30b04167574805.5b3e42ef3d364.jpg",
+    "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/e601ef67574805.5b3e42ef3e2cb.jpg",
+    "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/e8cd9b67574805.5b3e42ef3de3b.jpg"
+]
 
-book1.innerHTML = `<img src="${img1}"/>`;
-book2.innerHTML = `<img src="${img2}"/>`;
-book3.innerHTML = `<img src="${img3}"/>`;
+book1.innerHTML = `<img src="${imgUrls[0]}"/>`;
+book2.innerHTML = `<img src="${imgUrls[1]}"/>`;
+book3.innerHTML = `<img src="${imgUrls[2]}"/>`;
 
 footer.innerHTML = `<div class="footer-content">
                     <div class="footer-left">
